52ba5a2d789a638fcfe775a2322894f6e624f4f6
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2023-12-13 21:59:04 +0530

    "added react router"

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
+import LandingPage from './pages/LandingPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignUpPag from './pages/SignUpPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/signup' element={<SignUpPag/>}/>
+        <Route path='/home' element={<HomePage/>}/>
+      </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}>
+  </RouterProvider>,
 )
