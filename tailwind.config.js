22e0de20ea980cece0b71fd2b0bff19505b52b96
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2023-12-18 20:55:14 +0530

    "added new banner url"

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
         "netflix-gray" : "#232323"
       },
       backgroundImage: {
-        'hero': "url('https://assets.nflxext.com/ffe/siteui/vlv3/b4c7f092-0488-48b7-854d-ca055a84fb4f/5b22968d-b94f-44ec-bea3-45dcf457f29e/IN-en-20231204-popsignuptwoweeks-perspective_alpha_website_large.jpg')",
+        'hero': "url('https://assets.nflxext.com/ffe/siteui/vlv3/ca6a7616-0acb-4bc5-be25-c4deef0419a7/c5af601a-6657-4531-8f82-22e629a3795e/IN-en-20231211-popsignuptwoweeks-perspective_alpha_website_small.jpg')",
       },
     },
   },
