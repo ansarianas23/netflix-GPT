161f31b3c1a36b4a644fdce8a4e677b568f95eee
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2023-12-19 14:17:28 +0530

    "added keys in env file"

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,20 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyClWsDKmh3mwkC4LfqhXn4N8rbkZoSy4Uw",
-  authDomain: "netflix-gpt-325bc.firebaseapp.com",
-  projectId: "netflix-gpt-325bc",
-  storageBucket: "netflix-gpt-325bc.appspot.com",
-  messagingSenderId: "689916484862",
-  appId: "1:689916484862:web:8d685ba3f1b6cbe9f619af"
+  // apiKey: "AIzaSyClWsDKmh3mwkC4LfqhXn4N8rbkZoSy4Uw",
+  apiKey: String(import.meta.env.VITE_FIREBASE_API_KEY),
+  authDomain: String(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN),
+  projectId: String(import.meta.env.VITE_FIREBASE_PROJECT_ID),
+  storageBucket: String(import.meta.env.VITE_FIREBASE_STORAGE_BUCKET),
+  messagingSenderId: String(import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID),
+  appId: String(import.meta.env.VITE_FIREBASE_APP_ID),
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// auth function require for every firebase method like create acc update acc.
 export const auth = getAuth();
