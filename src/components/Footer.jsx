2f5e579936c356621f8c423d9aa57598d45c4359
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2023-12-21 01:59:32 +0530

    "fixed few bugs"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,22 +5,22 @@ const Footer = () => {
     <div className='w-full flex flex-col text-stone-300 px-8 py-5 lg:py-12 space-y-4'>
       <div>Questions? Call <span className='underline'>000-800-919-1694</span></div>
 
-      <ul className='grid grid-cols-2 lg:grid-cols-4 underline cursor-pointer gap-y-2 flex-wrap'>
-        <li>FAQ</li>
-        <li>Help Centre</li>
-        <li>Account</li>
-        <li>Media Centre</li>
-        <li>Investor Relations</li>
-        <li>Jobs</li>
-        <li>Ways to Watch</li>
-        <li>Terms of Use</li>
-        <li>Privacy</li>
-        <li>Cookie Preferences</li>
-        <li>Corporate Information</li>
-        <li>Contact Us</li>
-        <li>Speed Test</li>
-        <li>Legal Notices</li>
-        <li>Only on Netflix</li>
+      <ul className='grid grid-cols-2 lg:grid-cols-4 gap-y-2 flex-wrap'>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>FAQ</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Help Centre</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Account</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Media Centre</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Investor Relations</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Jobs</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Ways to Watch</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Terms of Use</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Privacy</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Cookie Preferences</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Corporate Information</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Contact Us</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Speed Test</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Legal Notices</li>
+        <li className='hover:underline cursor-pointer hover:text-red-500 transition-all w-fit delay-75'>Only on Netflix</li>
       </ul>
 
       <select className='w-fit bg-transparent text-white px-6 rounded-sm border-gray-700 border-[1px] '>
